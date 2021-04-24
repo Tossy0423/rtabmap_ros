77ecafdfd66de047ad281a8d70e0dadde83cea07
Tossy <syun111@gmail.com>
2021-04-24 18:58:13 +0900

    <add> readme

--- RUN-ROSBAG_RECORD.sh
+++ RUN-ROSBAG_RECORD.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+<< COMMENTOUT
+Overview:
+----------
+RTAB-Map, ORB-SLAM2で使用することができる記録ファイル(rosbag)を作成するためのスクリプト.
+このスクリプトで生成されたrosbagファイルを使うことで, 任意のタイミング, 各種パラメータをチューニングしてからもう一度SLAMを実行することができるので, 現地へ行ってカメラを回す必要がなくなる. 
+このスクリプトで対応しているカメラは, RealSense D435, ZED Cameraの2種類.
+
+< !!!!! 注意 Caution !!!!! >
+RTAB-Map, ORB-SLAM2で扱うTopicは画像であるため, 長時間使用する際はファイルのサイズに注意.
+各カメラにおける単位時間あたりのファイルサイズは以下の通り.
+- RealSense D435    : 40MB/sec (2.4GB/min)
+- ZED Camera        : 200MB/sec(12GB/min)
+
+
+
+Usage:
+----------
+- RealSense D435
+$ roslaunch realsense2_camera rs_camera.launch align_depth:=true
+
+- ZED camera
+$ roslaunch zed_wrapper zed_no_tf.launch
+$ rosrun dynamic_reconfigure dynparam set zed_node depth_confidence 99
+$ rosrun dynamic_reconfigure dynparam set zed_node depth_texture_conf 90
+$ rosrun dynamic_reconfigure dynparam set zed_node depth_confidence 100
+
+
+
+Autor:
+----------
+onigirimal
+
+COMMENTOUT
 
 function checkdir()
 {   
@@ -14,7 +47,7 @@ function checkdir()
     if [ ! -d $1 ]; then
         echo "bagdataを保存するディレクトリが見つかりませんでした."
         echo "保存ディレクトリを作成します. ($_cmd)"
-        mkdir $_cmd
+        # mkdir $_cmd
     fi
 }
 
@@ -25,8 +58,8 @@ function checkdir()
 # echo "実行時の引数の数 >> ${#}"
 
 # 保存するディレクトリパス
-SAVE_DIRPATH="/mnt/ssd1000"
-# SAVE_DIRPATH=$PWD
+# SAVE_DIRPATH="/mnt/ssd1000"
+SAVE_DIRPATH=$PWD
 
 # 保存するディレクトリ名
 SAVE_DIRNAME="bagdata"
@@ -62,19 +95,40 @@ $SAVE_DIRPATH"
 
     echo "保存するファイル名: 
 ${NOW_DATE}_zed.bag"
+    
 
-    # 実行コマンド作成
-    _cmd="rosbag record \
+    echo "< !!!!! 注意 Caution !!!!! >"
+    echo "ZED Cameraは, bagファイルが大きくなります."
+    
+    # どのTopicを記録するか選択
+    read -n1 -p "
+    0: ORB-SLAM2 only 
+    1: RTAB-Map only
+    2: ORB-SLAM2 & RTAB-Map
+    " select_topic
+
+    if [[ $select_topic = [0] ]]; then
+        echo ""
+        echo "ORB-SLAM2のみで使用できるTopicを取得します."
+        echo "単位時間あたりのファイルサイズは, 100MB/sec(6GB/min)です."    
+        _cmd="rosbag record \
             /zed_node/left/image_rect_color \
             /zed_node/right/image_rect_color \
             /zed_node/left/camera_info \
-            /zed_node/rgb/image_rect_color \
-            /zed_node/depth/depth_registered \
-            /zed_node/rgb/camera_info \
             /tf \
             /tf_static \
             /clock \
-            -O ${SAVE_FILEPATH}/${NOW_DATE}_rtabmap_zed"
+            -O ${SAVE_FILEPATH}/${NOW_DATE}_ORBonly_zed"
+    
+    elif [[ $select_topic = [1] ]]; then
+        echo abort
+    elif [[ $select_topic = [2] ]]; then
+        echo hoge
+    fi
+
+
+    # 実行コマンド作成
+    
     echo "実行コマンド: \
     $_cmd"
     
@@ -83,7 +137,7 @@ ${NOW_DATE}_zed.bag"
 
     # 実行 
     source ~/.bashrc
-    $_cmd
+    # $_cmd
 
 
 
@@ -115,5 +169,5 @@ $_cmd"
     
     # 実行 
     source ~/.bashrc
-    $_cmd 
+    # $_cmd 
 fi
