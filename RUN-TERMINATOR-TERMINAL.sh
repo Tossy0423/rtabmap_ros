9ea5545141e5c27565984b4786ec197b961890b6
Tossy <syun111@gmail.com>
2021-05-17 19:14:04 +0900

    <add> config file copy

--- RUN-TERMINATOR-TERMINAL.sh
+++ RUN-TERMINATOR-TERMINAL.sh
@@ -2,8 +2,20 @@
 
 # terminatorを立ち上げるためのプログラム
 
-## RTAB-MAP用のTerminatorを立ち上げる
-terminator -l mapping_rtabmap2 --working-directory ${PWD}
 
+
+
+mkdir -p ~/.config/terminator/
+if [ ! -f ~/.config/terminator/config.backup ]; then
+  cp ~/.config/terminator/config ~/.config/terminator/config.backup
+fi
+
+cp ./terminator_config/config ~/.config/terminator/config
+
+
+if [ "${1}" = "zed" ]; then
+    terminator -l mapping_rtabmap2 --working-directory ${PWD} &
+    sleep 1
+fi 
 
 
