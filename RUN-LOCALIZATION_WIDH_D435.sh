5e2ac44e2add279b3041da3ef5815cb489a9ad33
robo19 <syun111@gmail.com>
2021-05-20 16:21:44 +0900

    <fix> visualizer option

--- RUN-LOCALIZATION_WIDH_D435.sh
+++ RUN-LOCALIZATION_WIDH_D435.sh
@@ -7,12 +7,12 @@ _cmd="roslaunch rtabmap_ros localization_d435.launch \
 depth_topic:=/camera/aligned_depth_to_color/image_raw \
 rgb_topic:=/camera/color/image_raw \
 camera_info_topic:=/camera/color/camera_info \
-approx_sync:=false \
-use_sim_time:=false \
+approx_sync:=true \
+use_sim_time:=true \
 rviz:=true \
 rtabmapviz:=false \
 localization:=true \
-database_path:=/home/robo20/rtabmap_ws/src/rtabmap_ros/2f.db \
+database_path:=${PWD}/data.db \
 "
 # 再生する時
 # rosbag play -r 1.0 -l --clock <bagfile_path>
