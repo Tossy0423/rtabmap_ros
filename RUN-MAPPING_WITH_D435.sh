4eca82c5ba256d9ff16753d833d3e26f89518d41
robo19 <syun111@gmail.com>
2021-05-20 16:22:58 +0900

    <Add>Terminator用の実行スクリプト

--- RUN-MAPPING_WITH_D435.sh
+++ RUN-MAPPING_WITH_D435.sh
@@ -1,40 +1,226 @@
 #!/bin/bash
 
-# Load setup.bash
-source ~/rtabmap_ws/devel/setup.bash
+<< COMMENTOUT
+Overview:
+----------
+RealSense Cameraを使ったRTAB-MAPによるSLAMを実行するときに実行するファイル.
 
-# 
-# readonly SETTING_FILE_PATH="Examples/RGB-D/D435.yaml"
-# echo "Setting file path            >>  ${SETTING_FILE_PATH}"
 
-# readonly SUBSCRIBE_IMAGE_COLOR="/camera/rgb/image_raw:=/camera/color/image_raw"
-# echo "Subscribe image color topic  >>  ${SUBSCRIBE_IMAGE_COLOR}"
 
-# readonly SUBSCRIBE_IMAGE_DEPTH="/camera/depth_registered/image_raw:=/camera/aligned_depth_to_color/image_raw"
-# echo "Subscribe image depth topic  >>  ${SUBSCRIBE_IMAGE_DEPTH}"
+Usage:
+----------
+1. Realsense CameraとROSのwrapperを起動
+    $ bash RUN-MAPPING_WITH_D435.sh launch_rs
+2.zed_wrapper_rosでのパラメータを再設定
+    $ bash RUN-MAPPING_WITH_ZED.sh change_params
+3. (option) RTAB-MAP用のrosbagを起動
+    $ bash RUN-MAPPING_WITH_ZED.sh rosbag
+4. RTAB-MAPを起動
+    $ bash RUN-MAPPING_WITH_ZED.sh slam
 
-# 実行コマンド作成
-# _cmd="rosrun ORB_SLAM2 RGBD Vocabulary/ORBvoc.txt ${SETTING_FILE_PATH} ${SUBSCRIBE_IMAGE_COLOR} ${SUBSCRIBE_IMAGE_DEPTH}"
 
-_cmd="roslaunch rtabmap_ros mapping_d435.launch \
+
+Autor:
+----------
+onigirimal
+
+COMMENTOUT
+
+
+
+
+
+## ZED Cameraを立ち上げる
+if [ "${1}" = "launch_rs" ]; then
+
+readonly CMD_LAUNCH_D435="roslaunch realsense2_camera rs_rgbd.launch align_depth:=true"
+
+PROMPT="
+----------------------------------------
+(1) RealSense Cameraを起動します.
+$ ${CMD_LAUNCH_D435}
+
+start   : s
+quit    : q
+----------------------------------------
+>> "
+
+while true; 
+  do
+    
+    # ユーザーからの入力を待つ
+    read -n 1 -s -p "${PROMPT}" input;
+    # 入力した文字を表示
+    echo "${input}"
+    
+    # quit
+    if [ ${input} = "q" ]; then
+      echo "quit"
+      break;
+    
+    # プログラムを実行
+    elif [ ${input} = "s" ]; then
+      echo "start"
+      source ~/realsense_ws/devel/setup.bash
+      echo "${CMD_LAUNCH_D435}"  
+      sleep 1
+      ${CMD_LAUNCH_D435}
+    fi  
+ 
+  done
+
+
+
+
+
+
+## ZED Cameraのパラメータを再設定
+elif [ "${1}" = "launch_rs2" ]; then
+
+readonly CMD_LAUNCH_D435="roslaunch realsense2_camera rs_rgbd2.launch align_depth:=true"
+
+PROMPT="
+----------------------------------------
+(1) RealSense Cameraを起動します.
+$ ${CMD_LAUNCH_D435}
+
+start   : s
+quit    : q
+----------------------------------------
+>> "
+
+while true; 
+  do
+    
+    # ユーザーからの入力を待つ
+    read -n 1 -s -p "${PROMPT}" input;
+    # 入力した文字を表示
+    echo "${input}"
+    
+    # quit
+    if [ ${input} = "q" ]; then
+      echo "quit"
+      break;
+    
+    # プログラムを実行
+    elif [ ${input} = "s" ]; then
+      echo "start"
+      source ~/realsense_ws/devel/setup.bash
+      echo "${CMD_LAUNCH_D435}"  
+      sleep 1
+      ${CMD_LAUNCH_D435}
+    fi  
+ 
+  done
+
+
+
+
+## rosbag
+elif [ "${1}" = "rosbag" ]; then
+readonly CMD_ROSBAG="bash RUN-ROSBAG_RECORD.sh rs"
+# echo "${CMD_ROSBAG}"
+PROMPT="
+----------------------------------------
+(3) rosbagを. 
+$ ${CMD_ROSBAG}
+
+start   : s
+quit    : q
+----------------------------------------
+>> "
+
+while true; 
+  do
+    
+    # ユーザーからの入力を待つ
+    read -n 1 -s -p "${PROMPT}" input;
+    # 入力した文字を表示
+    echo "${input}"
+    
+    # quit
+    if [ ${input} = "q" ]; then
+      echo "break"
+      break;
+    
+    # プログラムを実行
+    elif [ ${input} = "s" ]; then
+      echo "start"
+      ${CMD_ROSBAG}
+      sleep 1
+    fi  
+ 
+  done
+
+
+
+
+
+
+
+## RTAB-MAPを起動
+elif [ "${1}" = "slam" ]; then
+
+#
+
+<< COMMENTOUT
+readonly CMD_SLAM="roslaunch rtabmap_ros mapping_zed.launch \
 rtabmap_args:="--delete_db_on_start" \
+database_path:=${PWD}/data.db \
+rgb_topic:=/zed_node/rgb/image_rect_color \
+depth_topic:=/zed_node/depth/depth_registered \
+camera_info_topic:=/zed_node/rgb/camera_info \
+frame_id:=base_link \
+approx_sync:=false \
+use_sim_time:=true \
+rviz:=true"
+COMMENTOUT
+
+readonly CMD_SLAM="roslaunch rtabmap_ros mapping_d435.launch \
+rtabmap_args:="--delete_db_on_start" \
+database_path:=${PWD}/realsense.db \
 depth_topic:=/camera/aligned_depth_to_color/image_raw \
 rgb_topic:=/camera/color/image_raw \
 camera_info_topic:=/camera/color/camera_info \
 approx_sync:=false \
 use_sim_time:=true \
-rviz:=true \
+rviz:=false \
 rtabmapviz:=true
 "
-# 再生する時
-# rosbag play -r 1.0 -l --clock <bagfile_path>
 
 
-echo "実行コマンド
-${_cmd}"
+PROMPT="
+----------------------------------------
+(4) RTAB-MAPを開始します. 
+$ ${CMD_SLAM}
 
-sleep 1
+start   : s
+quit    : q
+----------------------------------------
+>> "
 
-# 実行
-$_cmd
+while true; 
+  do
+    
+    # ユーザーからの入力を待つ
+    read -n 1 -s -p "${PROMPT}" input;
+    # 入力した文字を表示
+    echo "${input}"
+    
+    # quit
+    if [ ${input} = "q" ]; then
+      echo "break"
+      break;
+    
+    # プログラムを実行
+    elif [ ${input} = "s" ]; then
+      echo "start"
+      source ../../devel/setup.bash
+      ${CMD_SLAM}
+      sleep 1
+    fi  
+ 
+  done
 
+
+fi
